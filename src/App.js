f204c7a004751705552fa57ca71362ab551d10fb
Hunter <hunterkb@ksu.edu>
2021-01-08 16:20:22 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const KEYBINDINGS = {
+  q: "kick",
+  w: "snare",
+  e: "tom",
+  a: "conga",
+  s: "closed-hat",
+  d: "open-hat",
+  z: "cowbell",
+  x: "clav",
+  c: "rim"
+}
+
+class DrumMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sound: "",
+    };
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
+  
+  handleClick(sound) {
+    this.setState({sound: sound});
+    let url = `audio/${sound}.wav`;
+    new Audio(url).play();
+  }
+
+  handleKeyDown(e) {
+    let keyBindings = this.props.keyBindings;
+    if (Object.keys(keyBindings).includes(e.key)) {
+      document.querySelector(`button#${e.key}`).focus();
+      let sound = keyBindings[e.key];
+      this.setState({sound: sound});
+      let url = `audio/${sound}.wav`;
+      new Audio(url).play();
+    }
+  }
+
+  render() {
+    let keyBindings = this.props.keyBindings;
+
+    return (
+      <div id="drum-machine">
+        <p>{this.state.sound}</p>
+        <div id="drum-pad">
+          {Object.entries(keyBindings).map(([k, v]) => (
+            <button onClick={() => this.handleClick(v)}
+                    id={k}
+                    key={k}>
+              {k}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <DrumMachine keyBindings={KEYBINDINGS} /> 
+      );
 }
 
 export default App;
